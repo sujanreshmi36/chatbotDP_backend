f4c9a9c70a6fec48dedd6df685c037c530afd47f
sujanreshmi36 <sujan.reshmi.7@gmail.com>
2024-08-22 15:27:41 +0545

    edited in get-prompt api

--- src/modules/knowledge/knowledge.service.ts
+++ src/modules/knowledge/knowledge.service.ts
@@ -1,4 +1,11 @@
-import { BadRequestException, ConflictException, ForbiddenException, Inject, Injectable, NotFoundException } from '@nestjs/common';
+import {
+  BadRequestException,
+  ConflictException,
+  ForbiddenException,
+  Inject,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { CreateKnowledgeDto } from './dto/create-knowledge.dto';
 import { UpdateKnowledgeDto } from './dto/update-knowledge.dto';
 import { InjectRepository } from '@nestjs/typeorm';
@@ -28,28 +35,32 @@ export class KnowledgeService {
     @InjectRepository(Prompt)
     private promptRepo: Repository<Prompt>,
 
-    private config: ConfigService
+    private config: ConfigService,
+  ) {}
 
-  ) { }
-
-
-
-  async create(createKnowledgeDto: CreateKnowledgeDto, pdfFile?: Express.Multer.File) {
+  async create(
+    createKnowledgeDto: CreateKnowledgeDto,
+    pdfFile?: Express.Multer.File,
+  ) {
     try {
-      const isUser = await this.userRepo.findOne({ where: { id: createKnowledgeDto.userId } })
+      const isUser = await this.userRepo.findOne({
+        where: { id: createKnowledgeDto.userId },
+      });
       if (!isUser) {
-        throw new ForbiddenException("Invalid userId")
+        throw new ForbiddenException('Invalid userId');
       }
       //creating knowledge
-      const knowledgeModel = new Knowledge()
+      const knowledgeModel = new Knowledge();
       if (pdfFile) {
         const pdfContent = await pdfParse.default(pdfFile.buffer);
         if (!pdfContent.metadata) {
-          throw new BadRequestException("Invalid data form");
+          throw new BadRequestException('Invalid data form');
         }
         const maxLength = 5000;
         if (pdfContent.text.length > maxLength) {
-          throw new BadRequestException(`The PDF content is too long. Maximum allowed length is ${maxLength} characters.`);
+          throw new BadRequestException(
+            `The PDF content is too long. Maximum allowed length is ${maxLength} characters.`,
+          );
         }
         knowledgeModel.paragraph = pdfContent.text.replace(/\s+/g, ' ').trim();
       } else if (createKnowledgeDto.paragraph) {
@@ -64,18 +75,16 @@ export class KnowledgeService {
       if (e instanceof ForbiddenException || e instanceof BadRequestException) {
         throw e;
       }
-      throw new BadRequestException("Invalid file format");
+      throw new BadRequestException('Invalid file format');
     }
-
   }
-
 
   async ask(askQuesDto: askQuesDTO) {
     try {
-      const { userId, prompt, sessionId } = askQuesDto
+      const { userId, prompt, sessionId } = askQuesDto;
       const isuser = await this.userRepo.findOne({ where: { id: userId } });
       if (!isuser) {
-        throw new NotFoundException("User doesnot exist");
+        throw new NotFoundException('User doesnot exist');
       }
       //  const isApi=await this.apiRepo.findOne({where:{user:isuser}});
       //  if(!isApi.status){
@@ -87,12 +96,14 @@ export class KnowledgeService {
         throw new BadRequestException('API key is missing');
       }
       const genAI = new GoogleGenerativeAI(api_key);
-      const model = genAI.getGenerativeModel({ model: "gemini-pro" });
-      const knowledge = await this.knowledgeRepo.find({ where: { user: isuser } });
-      const paragraph = knowledge.map(knowledge => knowledge.paragraph);
+      const model = genAI.getGenerativeModel({ model: 'gemini-pro' });
+      const knowledge = await this.knowledgeRepo.find({
+        where: { user: isuser },
+      });
+      const paragraph = knowledge.map((knowledge) => knowledge.paragraph);
       if (!prompt) {
         return {
-          message: "Please Enter prompt"
+          message: 'Please Enter prompt',
         };
       }
       // const question = `forget my all prompt. I will give you some paragraphs of content . you just have a look and i will ask you question related to them later which you need to answer me. remember just give answer in full sentence such that you like assistant according to question based on the paragraph. if answer is not found there in paragraph just say some invalid response. paragraph goes like this remember it: ${paragraph} and question is ${prompt}`;
@@ -104,11 +115,11 @@ export class KnowledgeService {
         savePrompt.prompt = prompt;
         savePrompt.sessionId = sessionId;
         savePrompt.user = isuser;
-        savePrompt.response = response.text()
+        savePrompt.response = response.text();
         await this.promptRepo.save(savePrompt);
         return {
-          answer: response.text()
-        }
+          answer: response.text(),
+        };
       }
     } catch (e) {
       // return {
@@ -123,27 +134,24 @@ export class KnowledgeService {
     try {
       const isUser = await this.userRepo.findOne({ where: { id: userid } });
       if (!isUser) {
-        throw new NotFoundException("user not found.");
+        throw new NotFoundException('user not found.');
       }
       const isPrompt = await this.promptRepo.find({ where: { user: isUser } });
       if (!isPrompt) {
         return;
       }
-      const prompts = isPrompt.map(isPrompt => isPrompt.prompt);
-      const id = isPrompt.map(isPrompt => isPrompt.id);
-      const response = isPrompt.map(isPrompt => isPrompt.response);
-      const userId = isPrompt.map(isPrompt => isPrompt.user);
+      const prompts = isPrompt.map((isPrompt) => isPrompt.prompt);
+      const id = isPrompt.map((isPrompt) => isPrompt.id);
+      const response = isPrompt.map((isPrompt) => isPrompt.response);
       return {
         prompts: prompts,
         id: id,
         response: response,
-        userId: userId
-      }
+      };
     } catch (e) {
       throw new BadRequestException(e.message);
     }
   }
-
 
   //generate session id
   async generate() {
@@ -153,20 +161,20 @@ export class KnowledgeService {
     } catch (e) {
       throw new BadRequestException(e.message);
     }
-
   }
-
 
   //get-history
   async getHistroy(sessionID: string) {
     try {
-      const prompts = await this.promptRepo.find({ where: { sessionId: sessionID } });
+      const prompts = await this.promptRepo.find({
+        where: { sessionId: sessionID },
+      });
       if (!prompts) {
         return;
       }
-      const responses = prompts.map(prompt => ({
+      const responses = prompts.map((prompt) => ({
         prompt: prompt.prompt,
-        response: prompt.response
+        response: prompt.response,
       }));
       return { responses };
     } catch (e) {
@@ -179,9 +187,9 @@ export class KnowledgeService {
     try {
       const user = await this.userRepo.findOne({
         where: {
-          id: uid
-        }
-      })
+          id: uid,
+        },
+      });
       const knowledge = await this.knowledgeRepo.find({ where: { user } });
       if (!knowledge) {
         return;
@@ -190,18 +198,18 @@ export class KnowledgeService {
     } catch (e) {
       throw new BadRequestException(e.message);
     }
-
   }
 
   async update(updateDto: UpdateKnowledgeDto) {
-
     try {
       const { userId, id, paragraph, category } = updateDto;
       const user = await this.userRepo.findOne({ where: { id: userId } });
       if (!user) {
-        throw new ForbiddenException("Invalid userId")
+        throw new ForbiddenException('Invalid userId');
       }
-      const isExistingKnowledge = await this.knowledgeRepo.findOne({ where: { user: user, id: id } });
+      const isExistingKnowledge = await this.knowledgeRepo.findOne({
+        where: { user: user, id: id },
+      });
       isExistingKnowledge.paragraph = paragraph;
       isExistingKnowledge.category = category;
       return this.knowledgeRepo.save(isExistingKnowledge);
@@ -214,14 +222,14 @@ export class KnowledgeService {
     try {
       const user = await this.userRepo.findOne({ where: { id: userId } });
       if (!user) {
-        throw new ForbiddenException("Invalid userId")
+        throw new ForbiddenException('Invalid userId');
       }
-      const isExistingKnowledge = await this.knowledgeRepo.findOne({ where: { user: user, id: id } });
+      const isExistingKnowledge = await this.knowledgeRepo.findOne({
+        where: { user: user, id: id },
+      });
       return await this.knowledgeRepo.remove(isExistingKnowledge);
-
     } catch (e) {
       throw new BadRequestException(e.message);
     }
   }
-
 }
