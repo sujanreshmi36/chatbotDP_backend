2620ac0776a20d39765ae3979570609a0b39ab12
sujanreshmi36 <sujan.reshmi.7@gmail.com>
2024-08-22 15:50:43 +0545

    synchronize turned into false

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,22 +9,28 @@ import { ApiModule } from './modules/api/api.module';
 import { EnquiryModule } from './modules/enquiry/enquiry.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(),
-  TypeOrmModule.forRootAsync({
-    imports: [ConfigModule],
-    inject: [ConfigService],
-    useFactory: (configService: ConfigService) => ({
-      type: 'postgres',
-      host: configService.get('DB_HOST'),
-      port: +configService.get('DB_PORT'),
-      username: configService.get('DB_USERNAME'),
-      password: configService.get('DB_PASSWORD'),
-      database: configService.get('DB_NAME'),
-      synchronize: true,
-      logging: false,
-      entities: [join(process.cwd(), 'dist/**/*.entity.js')],
-    })
-  }), UsersModule, AuthModule, KnowledgeModule, ApiModule, EnquiryModule],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('DB_HOST'),
+        port: +configService.get('DB_PORT'),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_NAME'),
+        synchronize: false,
+        logging: false,
+        entities: [join(process.cwd(), 'dist/**/*.entity.js')],
+      }),
+    }),
+    UsersModule,
+    AuthModule,
+    KnowledgeModule,
+    ApiModule,
+    EnquiryModule,
+  ],
 })
-export class AppModule {
-}
+export class AppModule {}
